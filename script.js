952c9fbe111c3bd60a490531b251c87b8ac307d3
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-01-01 21:11:32 +0530

    update

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 const itemNameInput = document.getElementById('item-name-input');
 const itemPriceInput = document.getElementById('item-price-input');
 const addButton = document.getElementById('add');
-const shoppingList = document.getElementById('shopping-list');
-const totalHeading = document.getElementById('total');
+const shoppingListTable = document.getElementById('table1');
+const totalDiv = document.getElementById('total');
 
 let grandTotal = 0;
 
 addButton.addEventListener('click', () => {
-    const itemName = itemNameInput.value;
-    const itemPrice = itemPriceInput.value;
+  const itemName = itemNameInput.value;
+  const itemPrice = itemPriceInput.value;
 
-    if (!itemName || !itemPrice) {
-        return; // invalid input, do nothing
-    }
+  if (!itemName || !itemPrice) {
+    return; // invalid input, do nothing
+  }
 
-    grandTotal += parseFloat(itemPrice);
+  grandTotal += parseFloat(itemPrice);
 
-    const row = document.createElement('tr');
-    const itemColumn = document.createElement('td');
-    itemColumn.textContent = itemName;
-    row.appendChild(itemColumn);
-    const priceColumn = document.createElement('td');
-    priceColumn.textContent = itemPrice;
-    row.appendChild(priceColumn);
-    shoppingList.appendChild(row);
+  // create new table row with item name, price, and total
+  const row = document.createElement('tr');
+  const itemColumn = document.createElement('td');
+  itemColumn.textContent = itemName;
+  row.appendChild(itemColumn);
+  const priceColumn = document.createElement('td');
+  priceColumn.textContent = itemPrice;
+  row.appendChild(priceColumn);
+  const totalColumn = document.createElement('td');
+  totalColumn.textContent = itemPrice;
+  row.appendChild(totalColumn);
+  shoppingListTable.appendChild(row);
 
-    totalHeading.textContent = grandTotal;
+  // update grand total
+  totalDiv.textContent = `Total Amount is ${grandTotal} $`;
 
-    // clear input fields
-    itemNameInput.value = '';
-    itemPriceInput.value = '';
+  // clear input fields
+  itemNameInput.value = '';
+  itemPriceInput.value = '';
 });
